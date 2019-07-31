0f6434bcd61b04da1588dec720711033d64fce97
Raphael Ponce <raphael.fleury@b2wdigital.com>
2019-07-31 11:08:33 -0300

    Iniciando modulo03

---
+++ modulo03/app03/src/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = () => (
+  <ul>
+    <li>Fazer café</li>
+    <li>Estudar React</li>
+    <li>Entrar na comunidade da Rocketseat</li>
+  </ul>
+);
+
+export default TodoList;
