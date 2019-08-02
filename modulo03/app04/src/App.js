02d935124aac134efbe85795b0cd76e1303b7db1
Raphael Ponce <raphael.fleury@b2wdigital.com>
2019-08-02 15:51:31 -0300

    Iniciando app04 do modulo 03

---
+++ modulo03/app04/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Provider } from "react-redux";
+import "./config/ReactotronConfig";
+import store from "./store";
+import TodoList from "./TodoList";
+
+console.tron.log("Testando");
+
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+
+export default App;
